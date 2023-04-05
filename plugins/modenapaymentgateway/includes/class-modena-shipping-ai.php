09c5fcf9b9c970182a924b2fc735bab5c491d3ab
“Sten <sten.uuesoo@gmail.com>
2023-04-05 11:56:23 +0300

    removed settings file bc had issues with initializing for other functions

--- plugins/modenapaymentgateway/includes/class-modena-shipping-ai.php
+++ plugins/modenapaymentgateway/includes/class-modena-shipping-ai.php
@@ -14,9 +14,10 @@ class WC_Estonia_Shipping_Method extends WC_Shipping_Method {
         $this->id                   = 'modena-shipping-itella-terminals';
         $this->method_title         = __('Itella pakiterminalid', 'woocommerce');
         $this->method_description   = __('Itella pakiterminalide lahendus Modenalt', 'woocommerce');
-
+        $this->title                = __('Itella pakiterminalid', 'woocommerce');
         $this->supports             = array('shipping-zones', 'instance-settings',);
-        $this->instance_form_fields = include __DIR__ . '/class-modena-shipping-settings.php';
+
+        $this->init_form_fields();
         $this->register_hooks();
 
     }
@@ -50,10 +51,11 @@ class WC_Estonia_Shipping_Method extends WC_Shipping_Method {
         static $rendered = false;
 
         if (!$rendered) {
-            $contactEmail = $this->get_option('sender_email');
-            $order = wc_get_order($order_id);
+            $contactEmail   = $this->get_option('sender_email');
+            $order          = wc_get_order($order_id);
             $orderReference = $order->get_order_number();
             $packageContent = '';
+            $total_weight   = 0;
 
             foreach ($order->get_items() as $item_id => $item) {
                 $product_name = $item->get_name();
@@ -371,4 +373,83 @@ class WC_Estonia_Shipping_Method extends WC_Shipping_Method {
         return 110;
     }
 
+    public function init_form_fields(): void
+    {
+        $cost_desc = __( 'Enter a cost (excl. tax) or sum, e.g. <code>10.00 * [qty]</code>.') . '<br/><br/>' . __( 'Use <code>[qty]</code> for the number of items, <br/><code>[cost]</code> for the total cost of items, and <code>[fee percent="10" min_fee="20" max_fee=""]</code> for percentage based fees.');
+
+        $this->instance_form_fields = array(
+            'title'            => array(
+                'title'       => __( 'Title', 'woocommerce' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
+                'default'     => $this->method_title,
+                'desc_tip'    => true,
+            ),
+            'cost'       => array(
+                'title'             => __( 'Cost'),
+                'type'              => 'text',
+                'placeholder'       => '',
+                'description'       => $cost_desc,
+                'default'           => '0',
+                'desc_tip'          => true,
+                'sanitize_callback' => array( $this, 'sanitize_cost' ),
+            ),
+            'free-shipping-treshold'      => array(
+                'title'       => __( 'Free shipping over sum of' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the amount needed for free shipping.'),
+                'default'     => __(0 ),
+                'desc_tip'    => true,
+            ),
+            'sender-name'       => array(
+                'title'       => __( 'Sender name' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the parcel sender name that is required and sent to Itella.'),
+                'default'     => __( ''),
+                'desc_tip'    => true,
+            ),
+            'sender-email'       => array(
+                'title'       => __( 'Sender email' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the parcel sender email that is required and sent to Itella.'),
+                'default'     => __( ''),
+                'desc_tip'    => true,
+            ),
+            'sender-phone'       => array(
+                'title'       => __( 'Sender phone' ),
+                'type'        => 'number',
+                'description' => __( 'This controls the parcel sender phone number that is required and sent to Itella.'),
+                'default'     => __('' ),
+                'desc_tip'    => true,
+            ),
+            'itella_api_key'      => array(
+                'title'       => __( 'Itella API Key' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the API key Secret from Modena'),
+                'default'     => __( 'thisistheitellaapikeyfor1444221!'),
+                'desc_tip'    => true,
+            ),
+            'itella_api_secret'      => array(
+                'title'       => __( 'Itella API Secret' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the API key Secret from Itella'),
+                'default'     => __( 'secretcustomerapisecretfromparterportal112.xxxa4!'),
+                'desc_tip'    => true,
+            ),
+            'client-id'      => array(
+                'title'       => __( 'Modena API ID' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the API key ID from Modena'),
+                'default'     => __( 'idofcustomersecretkeyfromparterportal112.yyyy66!'),
+                'desc_tip'    => true,
+            ),
+            'client-secret'      => array(
+                'title'       => __( 'Modena API Secret' ),
+                'type'        => 'text',
+                'description' => __( 'This controls the API key Secret from Modena'),
+                'default'     => __( 'secretcustomerapisecretfromparterportal112.xxxa4!'),
+                'desc_tip'    => true,
+            )
+        );
+    }
 }
