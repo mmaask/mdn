14e8944b140d8901b0b08de5c28c275b9176e2ac
“Sten <sten.uuesoo@gmail.com>
2023-06-27 16:54:01 +0300

    added error handling if there are two seperate plugin entities, so it does not crash the site.

--- plugins/trunk/modena.php
+++ plugins/trunk/modena.php
@@ -24,14 +24,32 @@ if (!defined('MODENA_PLUGIN_PATH')) {
 
 add_action('plugins_loaded', 'modena_init');
 
-function modena_init()
-{
-    static $modena_plugin;
+if (!function_exists('modena_init')) {
+    function modena_init()
+    {
+        static $modena_plugin;
 
-    if (!isset($modena_plugin)) {
-        require_once(MODENA_PLUGIN_PATH . 'includes/class-modena-init-handler.php');
-        $modena_plugin = new Modena_Init_Handler();
+        if (!isset($modena_plugin)) {
+            require_once(MODENA_PLUGIN_PATH . 'includes/class-modena-init-handler.php');
+            $modena_plugin = new Modena_Init_Handler();
+        }
+
+        $modena_plugin->run();
     }
+} else {
+    add_action('woocommerce_init', function() {
+        if (class_exists('WC_Logger')) {
+            $this->logger = new WC_Logger(array(new Modena_Log_Handler()));
+            $this->logger->error('Function modena_init has already been declared. Please delete deprecated versions of Modena plugin from this site and retry
+             reinstalling from the Wordpress Plugin store. modena_init is initialized already: ' . function_exists('modena_init'));
+        }
+    });
 
-    $modena_plugin->run();
+    add_action('admin_notices', function() {
+        ?>
+        <div class="notice notice-error">
+            <p><?php _e('Function modena_init has already been declared. Please delete deprecated versions of Modena plugin from this site and retry reinstalling from the Wordpress Plugin store.', 'modena'); ?></p>
+        </div>
+        <?php
+    });
 }
